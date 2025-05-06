d0250c3d701e83e7315874c00237fa44d9709304
simontijanic <simontijanicerkul@gmail.com>
2025-05-06 12:37:05 +0200

    Opacity image

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const stopButton = document.getElementById('stopButton');
 stopButton.addEventListener('click', () => {
     scrollingText.style.animationPlayState = 'paused';
     scrollingText.style.display = 'none'; // Remove the text when the button is pressed
+
+    // Set the background image opacity to 1
+    document.body.style.setProperty('--background-opacity', '1');
 });
