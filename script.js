0922a461c2be3bae010219018601d1d384ca3f14
simontijanic <simontijanicerkul@gmail.com>
2025-05-06 13:03:52 +0200

    rasisme

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ document.addEventListener('keypress', (event) => {
         inputField.focus();
     } else if (event.key === 'Enter' && inputField.style.display === 'block') {
         const userInput = inputField.value.trim().toLowerCase();
-        if (userInput === "im not a racist".toLowerCase() || userInput === "jeg er ikke rasist".toLowerCase()) {
+        if (userInput === "Rasisme er teit!".toLowerCase() ) {
             scrollingText.style.animationPlayState = 'paused';
             scrollingText.style.display = 'none'; 
-            inputField.style.display = 'none';
+            inputField.remove();
 
             document.body.style.setProperty('--background-opacity', '1');
         } else {
-            alert("Incorrect input. Please type: 'Im not a racist' or 'Jeg er ikke rasist'");
+            alert("FEIL!'");
         }
     }
 });
